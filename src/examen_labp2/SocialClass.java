09ced38566b9a1e3ff238b0455a9924afe7078bd
tommy-aki <141044473+tommy-aki@users.noreply.github.com>
2024-02-16 13:18:01 -0600

    Add files via upload

---
+++ src/examen_labp2/SocialClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen_labp2;
+
+/**
+ *
+ * @author Tommy Lee Pon
+ */
+public class SocialClass {
+    
+}
