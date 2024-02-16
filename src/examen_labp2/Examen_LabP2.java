389ee9d67dc4848ae782758704e1a7257b2f3e48
tommy-aki <141044473+tommy-aki@users.noreply.github.com>
2024-02-16 13:12:50 -0600

    Add files via upload

---
+++ src/examen_labp2/Examen_LabP2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen_labp2;
+
+/**
+ *
+ * @author Tommy Lee Pon
+ */
+public class Examen_LabP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
